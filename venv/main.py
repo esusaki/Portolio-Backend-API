f1031eb059bf1bca0fd4d58ea5baf5dda76cba82
esusaki <susaki2002@gmail.com>
2024-04-11 21:04:20 +0900

    転生git add .

--- venv/main.py
+++ venv/main.py
@@ -13,7 +13,13 @@ def get_user(user_id):
     # else:
     #     return jsonify({"error":"User not found"}), 404
     target_user = db.session.get(User, user_id)
-    return jsonify(func_like_user_schema(target_user)), 200
+
+    user_not_found = (target_user == None)
+
+    if user_not_found:
+        return jsonify({"message":"User Not Found. Check user_id."}), 404
+    else:
+        return jsonify(func_like_user_schema(target_user)), 200
 
 ## 指定されたuser_idのユーザーの投稿一覧の取得
 @app.route("/api/v1/posts/<user_id>", methods = ["GET"])
@@ -26,56 +32,74 @@ def get_posts(user_id):
 @app.route("/api/v1/post/<post_id>", methods = ["GET"])
 def get_post_info(post_id):
     target_post = db.session.get(Post, post_id)
-    target_post_info = {
-        "post_id" : post_id,
-        "user_id" : target_post.user_id,
-        "title" : target_post.title,
-        "description" : target_post.description,
-        "icon" : target_post.icon,
-        "created_date" : target_post.created_date,
-        "updated_date" : target_post.updated_date,
-        "username" : target_post.user.username,
-        "usericon" : target_post.user.photo_url
-    }
-    return jsonify(target_post_info)
+
+    post_not_found = (target_post == None)
+
+    if post_not_found:
+        return jsonify({"message":"Post Not Found. Check post_id."}), 404
+    else:
+        target_post_info = {
+            "post_id" : post_id,
+            "user_id" : target_post.user_id,
+            "title" : target_post.title,
+            "description" : target_post.description,
+            "icon" : target_post.icon,
+            "created_date" : target_post.created_date,
+            "updated_date" : target_post.updated_date,
+            "username" : target_post.user.username,
+            "usericon" : target_post.user.photo_url
+        }
+        return jsonify(target_post_info)
 
 #POST
 ## 新規ユーザーの作成
 @app.route("/api/v1/user", methods = ["POST"])
 def create_user():
-    new_user = User(
-        # bio, Xはデフォルトではnull値になる
-        user_id = request.json["user_id"],
-        username = request.json["username"],
-        photo_url = request.json["photo_url"]
-    )
-    db.session.add(new_user)
-    db.session.commit()
-    return jsonify(func_like_user_schema(new_user))
+    user_id_already_exists = (db.session.get(User, request.json["user_id"])!=None)
+
+    if user_id_already_exists:
+        return jsonify({"message":"user_id Already Exists."}), 403
+    else:
+        new_user = User(
+            # bio, Xはデフォルトではnull値になる
+            user_id = request.json["user_id"],
+            username = request.json["username"],
+            photo_url = request.json["photo_url"]
+        )
+        db.session.add(new_user)
+        db.session.commit()
+        return jsonify(func_like_user_schema(new_user))
 
 ## 新規投稿の作成
 @app.route("/api/v1/post/<user_id>", methods = ["POST"])
 def create_post(user_id):
-    new_post = Post(
-        user_id = user_id,
-        title = request.json["title"],
-        description = request.json["description"],
-        icon = request.json["icon"]
-        #URLs = request.json["URL"]
-    )
-    db.session.add(new_post)
-    db.session.commit()
-    # for url in URLs:
-    #     print(new_post.post_id)
-    #     print("aiueo")
-    #     new_url = URL(
-    #         URL = url, 
-    #         post_id = new_post.post_id
-    #     )
-    #     db.session.add(new_url)
-    # db.session.commit()
-    return jsonify(post_schema.dump(new_post))
-    # return "aiueo"
+    target_user = db.session.get(User, user_id)
+
+    user_not_found = (target_user == None)
+
+    if user_not_found:
+        return jsonify({"message":"User Not Found. Check user_id."}), 404
+    else:
+        new_post = Post(
+            user_id = user_id,
+            title = request.json["title"],
+            description = request.json["description"],
+            icon = request.json["icon"]
+            #URLs = request.json["URL"]
+        )
+        db.session.add(new_post)
+        db.session.commit()
+        # for url in URLs:
+        #     print(new_post.post_id)
+        #     print("aiueo")
+        #     new_url = URL(
+        #         URL = url, 
+        #         post_id = new_post.post_id
+        #     )
+        #     db.session.add(new_url)
+        # db.session.commit()
+        return jsonify(post_schema.dump(new_post))
+        # return "aiueo"
 
 # PUT
 ## 指定されたuser_idのユーザーの情報の更新
@@ -103,9 +127,15 @@ def update_post(post_id):
 @app.route("/api/v1/post/<post_id>", methods = ["DELETE"])
 def delete_post(post_id):
     target_post = db.session.get(Post, post_id)
-    db.session.delete(target_post)
-    db.session.commit()
-    return jsonify(post_schema.dump(target_post))
+
+    post_not_found = (target_post == None)
+
+    if post_not_found:
+        return jsonify({"message":"Post Not Found. Check post_id."}), 404
+    else:
+        db.session.delete(target_post)
+        db.session.commit()
+        return jsonify(post_schema.dump(target_post))
 
 
 if __name__ == "__main__":
